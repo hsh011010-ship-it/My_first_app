dbeed7880db42095c250908566610ac7f86c127e
hsh011010-ship-it <hsh011010@gmail.com>
2025-12-15 03:26:49 +0900

    Update data graph page

--- pages/02_데이터_그래프.py
+++ pages/02_데이터_그래프.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("데이터 그래프 페이지")
+
+'### :orange[Pandas 데이터프레임]'
+import pandas as pd
+df = pd.DataFrame(
+    {'id': [1, 2, 3],
+     'name': ['Alice', 'Bob', 'Charlie'],
+     'age': [24, 35, 45]
+    }
+)
+df   # 데이터프레임 출력
