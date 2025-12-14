9f57ac45f8313bbc6d7d8c5b4170aa3f11afc2c9
hsh011010-ship-it <hsh011010@gmail.com>
2025-12-15 02:57:46 +0900

    Add st.write examples to text & media page

--- pages/01_텍스트_미디어.py
+++ pages/01_텍스트_미디어.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("텍스트 미디어 페이지")
+
+'## 🔠: 일반 텍스트'
+
+st.title('제목 : st.title()')
+st.header('헤더 : st.header()')
+st.subheader('서브헤더 : st.subheader()')
+st.text('본문 텍스트 : st.text()')
+st.markdown('## 마크다운 : st.markdown()') 
+st.caption('캡션(작고 흐린 글씨로 표현됨) : st.caption()')
+
+'# 🔠: st.write()'
+st.write('# 마크다운 H1 : st.write()')
+st.write('### 마크다운 H3 : st.write()')
+st.write('')   # 빈 줄 추가
