d6572cac39df8ccb41bba8625a25966988bccff0
hsh011010-ship-it <hsh011010@gmail.com>
2025-12-15 05:06:05 +0900

    Update advanced features page

--- pages/05_고급기능.py
+++ pages/05_고급기능.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title("고급 기능")
+
+import time
+
+@st.cache_data
+def long_running_function(param1):
+    time.sleep(5)  
+    return param1 * param1
+
+start = time.time()
+
+# 숫자 입력은 입력된 값을 반환
+num_1 = st.number_input('입력한 숫자의 제곱을 계산합니다.')
+st.write(f'{num_1}의 제곱은 {long_running_function(num_1)}입니다.' +
+         f'계산시간은 {time.time() - start:.2f}초 소요')
+st.write(' :green[캐싱이 적용되면 동일한 계산은 저장된 결과를 사용하여 빠르게 처리함]')
