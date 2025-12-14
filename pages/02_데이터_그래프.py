c859e30041bc1dd09a3e8cc35199f1a0308a2577
hsh011010-ship-it <hsh011010@gmail.com>
2025-12-15 03:33:37 +0900

    Update data graph page

--- pages/02_데이터_그래프.py
+++ pages/02_데이터_그래프.py
@@ -18,3 +18,32 @@ col1, col2, col3 = st.columns(3)   # 3개의 컬럼 생성
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+
+
+'# :blue[Streamlit 그래프]'
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+    )
+
+'#### :orange[st.area_chart()]'
+st.area_chart(chart_data)
+
+'#### :orange[st.line_chart()]'
+st.line_chart(chart_data)
+
+'#### :orange[st.bar_chart()]'
+st.bar_chart(chart_data)
+
+'#### :orange[st.scatter_chart()]'
+st.scatter_chart(chart_data)
+
+'#### :orange[st.map()]'
+df = pd.DataFrame(
+    np.random.randn(100, 2) / [100, 100] + [37.55, 126.92],
+    colums= ['lat', 'lon'],
+)
+st.map(df)
